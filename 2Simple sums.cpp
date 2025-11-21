09ecd232be670a0fce52747473ce51bb88a1573b
alexskotland <alexrabotapetkov@gmail.com>
2025-11-21 14:18:16 +0000

    Add files via upload

---
+++ 2Simple sums.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                             aleksandar petkov 
+                             simple sums
+                             
+
+*******************************************************************************/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 8;          // first variable
+    int b = 10;         // second variable
+    int sum = a + b;    // third variable adds them together
+
+    cout << sum;        // output result
+    return 0;
+}
