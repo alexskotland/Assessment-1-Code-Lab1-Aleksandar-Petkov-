6b0734e5896dc632935ae79bcb32bea6362bcebc
alexskotland <alexrabotapetkov@gmail.com>
2025-11-21 16:50:22 +0000

    Add files via upload

---
+++ 3 BIOGRAPHY .cpp
@@ -0,0 +1,18 @@
+/*****************************************************************************
+ * **********ALEKSANDAR PETKOV************************************************
+ * **********BIOGRAPHY*******************************************************/
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "NAME:ALEKSANDAR PETKOV";
+    string hometown = "HOME TOWN:PLOVDIV";
+    string age = "age:42";
+
+    cout << name << endl << hometown << endl << age << endl;
+
+    return 0;
+}
